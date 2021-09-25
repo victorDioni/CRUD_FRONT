d66317370423383b4161d44e295aefc22781b653
Victor Dionizio <victor.dionizio28@gmail.com>
2021-09-25 18:07:28 -0300

    Criando viwes

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 // css/style.css
 
 app.get('/',(req, res)=>{
-    res.send("Crud Application");
+    res.render('index')
 })
 
 app.listen(8080, ()=> {console.log('Server is running on http://localhost:${8080}')})
