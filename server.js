a4d941d5e6309359a2a1fa0b64d69edea6b2a376
Victor Dionizio <victor.dionizio28@gmail.com>
2021-09-25 17:09:50 -0300

    Configurando modulos

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const dotenv = require('dontenv');
+const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -10,8 +12,21 @@ const PORT = process.env.PORT || 8080
 //log requests
 app.use(morgan('tiny'));
 
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended : true}))
+
+//set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+// css/style.css
+
 app.get('/',(req, res)=>{
     res.send("Crud Application");
 })
 
-app.listen(PORT, ()=> {console.log('Server is running on http://localhost:${PORT}')})
+app.listen(8080, ()=> {console.log('Server is running on http://localhost:${8080}')})
