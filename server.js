6b1164a828d686b5461786a79edfc1c6986c2ce9
Victor Dionizio <victor.dionizio28@gmail.com>
2021-10-02 17:11:28 -0300

    Implementando e configurando as rotas de conexoes

--- server.js
+++ server.js
@@ -25,16 +25,8 @@ app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 // css/style.css
 
-app.get('/',(req, res)=>{
-    res.render('index')
-})
+// LOAD ROUTERS
 
-app.get('/add-user',(req, res)=>{
-    res.render('add_user')
-})
-
-app.get('/update-user',(req, res)=>{
-    res.render('update_user')
-})
+app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> {console.log('Server is running on http://localhost:${PORT}')})
